dd3c77a14aa387b9df59157b8a9ee87c912114fd
Kwiatek <100546273+xKwiateq@users.noreply.github.com>
2022-12-15 08:43:37 +0000

    gowno

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ import Nav from "./Nav";
 
 const Header = () => {
   return (
-    <div className="w-full h-screen bg-cover bg-">
+    <div className="w-full h-screen bg-cover ">
       <Nav />
     </div>
   );
