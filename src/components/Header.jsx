e672b133feb9e6c2215df1563b0f1a1739faff0b
Kwiatek <100546273+xKwiateq@users.noreply.github.com>
2022-12-15 08:35:09 +0000

    fff

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Nav from "./Nav";
+
+const Header = () => {
+  return (
+    <div className="w-full h-screen bg-cover bg-">
+      <Nav />
+    </div>
+  );
+};
+
+export default Header;
